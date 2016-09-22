fb15422b7efa8b9034fd5eab3194eddba4c43a10
vipex <luca.ravizzotti@gmail.com>
2016-09-22 18:43:21 +0200

    Create mock server and data

---
+++ tests/mock.js
@@ -0,0 +1,51 @@
+'use strict';
+var nock = require('nock');
+var data = require('./mock-data.json');
+
+module.exports.createAPIMock = _createAPIMock;
+function _createAPIMock() {
+	var headers = {"Content-Type": "application/json"};
+	var server = nock(data.config.server + '/api');
+
+	// Base test
+	server.get('/').reply(200, {}, headers);
+
+	// Tokens
+	server.post('/tokens').reply(function (u, r, cb) {
+		if (r.username && r.password) {
+			if (r.username === data.config.username && r.password === data.config.password) {
+				return cb(null, [200, data.endpoints.tokens.token, headers]);
+			} else {
+				return cb(null, [401, data.endpoints.tokens['401'], headers]);
+			}
+		} else {
+			return cb(null, [400, data.endpoints.tokens['400'], headers]);
+		}
+	});
+	server.delete('/tokens').reply(function (u, r, cb) {
+		if (u.split('/').length > 1) {
+			return cb(null, [200, '', headers]);
+		} else {
+			if (u.split('/')[1] === data.endpoints.tokens.token.token) {
+				return cb(null, [200, '', headers]);
+			} else {
+				return cb(null, [500, data.endpoints.tokens['500'], headers]);
+			}
+		}
+	});
+
+	return server;
+}
+
+module.exports.createBaseMock = _createBaseMock;
+function _createBaseMock() {
+	var headers = {"Content-Type": "application/json"};
+	var server = nock(data.config.server + '/api');
+
+	// Base test
+	server.get('/').reply(200, {}, headers);
+	server.post('/').reply(function (u, r, cb) { return cb(null, [200, r, headers]); });
+	server.put('/').reply(function (u, r, cb) { return cb(null, [200, r, headers]); });
+	server.patch('/').reply(function (u, r, cb) { return cb(null, [200, r, headers]); });
+	server.delete('/').reply(200);
+}
